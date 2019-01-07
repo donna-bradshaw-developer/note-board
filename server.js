6971b725526eacc35f1d76c2f7f7c6807ace62eb
Donna <donna.bradshaw.developer@gmail.com>
2019-01-07 05:34:10 -0500

    minor changes

--- server.js
+++ server.js
@@ -11,10 +11,13 @@ const noteRoutes = require('./routes/noteRoutes')
 
 const PORT = process.env.PORT || 5510
 
+const path = require("path")
+
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use(morgan('dev'))
 app.use(express.json())
 
-mongoose.connect('mongodb://localhost:27017/myboardapp', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/myboardapp', { useNewUrlParser: true })
     .then(()=> console.log('Successfully Connected to MongoDB'))
     .catch(err => console.log(err))
 
@@ -31,6 +34,10 @@ app.use('/api', expressJwt({secret: process.env.SECRET}))
 app.use('/auth', authRoutes)
 app.use('/api/profile', profileRoutes)
 app.use('/api/note', noteRoutes)
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server is listening on port ${PORT}`)
